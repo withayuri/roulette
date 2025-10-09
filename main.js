eeba01ae51f8aefe25fbb2c3c533ecaa87437c38
ayuri <ringoayu713@gmail.com>
2025-10-09 19:24:34 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+'use script'
+
+{
+    const btn = document.getElementById('btn');
+    const menu = document.getElementById('result');
+
+    btn.addEventListener('click', () => {
+        const results = ['ハンバーグ', '唐揚げ', 'チキン南蛮', 'オムライス', '焼きそば', 'カレー', '生姜焼き', 'ミートパスタ'];
+        const n = Math.floor(Math.random() * results.length);
+        result.textContent = results[n];
+    });
+}
